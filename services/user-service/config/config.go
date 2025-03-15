37e0fa2f6deeadac920cc2433a559852c167efb1
annguyen0511 <nguyentrongan051102@gmail.com>
2025-03-15 11:22:28 +0700

    update: services config

---
+++ services/user-service/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	ServerPort string
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	JWTSecret  string
+}
+
+func LoadConfig() (*Config, error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./config")
+	viper.AutomaticEnv()
+
+	var cfg Config
+	if err := viper.ReadInConfig(); err != nil {
+		return &cfg, err
+	}
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return &cfg, err
+	}
+	return &cfg, nil
+}
